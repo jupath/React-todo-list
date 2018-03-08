bdcab101feba28193c38600c950e7dda14358592
Jupath <attila.taligas@gmail.com>
2018-03-08 16:29:50 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>React TODO list</h1>
+      </div>
+    )
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'public', 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.*s?css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: { sourceMap: true }
+            },
+            {
+              loader: 'sass-loader',
+              options: { sourceMap: true }
+            }
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    publicPath: '/dist/'
+  }
+}
